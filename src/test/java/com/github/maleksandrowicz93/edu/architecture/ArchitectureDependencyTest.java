0fc29b74dbbf2433c879f3dbea0e4e518a31ce1d
maleksandrowicz93 <mateusz.aleksandrowicz93@gmail.com>
2024-10-14 22:06:30 +0200

    extend arch test with products contexts layers

--- src/test/java/com/github/maleksandrowicz93/edu/architecture/ArchitectureDependencyTest.java
+++ src/test/java/com/github/maleksandrowicz93/edu/architecture/ArchitectureDependencyTest.java
@@ -37,4 +37,50 @@ public class ArchitectureDependencyTest {
                              .whereLayer("educationalInstitution").mayOnlyAccessLayers("availability", "inventory")
                              .check(classes);
     }
+
+    @Test
+    void checkEducationalInstitutionDependencies() {
+        layeredArchitecture().consideringOnlyDependenciesInLayers()
+                             .layer("courseCatalog").definedBy("com.github.maleksandrowicz93.edu.domain.educationalInstitution.courseCatalog..")
+                             .layer("courseCreation").definedBy("com.github.maleksandrowicz93.edu.domain.educationalInstitution.courseCreation..")
+                             .layer("courseLeadership").definedBy("com.github.maleksandrowicz93.edu.domain.educationalInstitution.courseLeadership..")
+                             .layer("facultyCatalog").definedBy("com.github.maleksandrowicz93.edu.domain.educationalInstitution.facultyCatalog..")
+                             .layer("facultyCreation").definedBy("com.github.maleksandrowicz93.edu.domain.educationalInstitution.facultyCreation..")
+                             .layer("inventory").definedBy("com.github.maleksandrowicz93.edu.domain.educationalInstitution.inventory..")
+                             .layer("professorCatalog").definedBy("com.github.maleksandrowicz93.edu.domain.educationalInstitution.professorCatalog..")
+                             .layer("professorEmployment").definedBy("com.github.maleksandrowicz93.edu.domain.educationalInstitution.professorEmployment..")
+                             .layer("shared").definedBy("com.github.maleksandrowicz93.edu.domain.educationalInstitution.shared..")
+                             .layer("studentEnrollment").definedBy("com.github.maleksandrowicz93.edu.domain.educationalInstitution.studentEnrollment..")
+                             .whereLayer("shared").mayNotAccessAnyLayer()
+                             .whereLayer("courseCatalog").mayOnlyAccessLayers("shared")
+                             .whereLayer("facultyCatalog").mayOnlyAccessLayers("shared")
+                             .whereLayer("professorCatalog").mayOnlyAccessLayers("shared")
+                             .whereLayer("inventory").mayOnlyAccessLayers("shared")
+                             .whereLayer("facultyCreation").mayOnlyAccessLayers("shared", "inventory", "facultyCatalog")
+                             .whereLayer("courseLeadership").mayOnlyAccessLayers("shared", "inventory", "courseCatalog", "professorCatalog")
+                             .whereLayer("studentEnrollment").mayOnlyAccessLayers("shared", "inventory", "facultyCatalog")
+                             .whereLayer("courseCreation").mayOnlyAccessLayers("shared", "inventory", "courseCatalog", "courseLeadership", "studentEnrollment")
+                             .whereLayer("professorEmployment").mayOnlyAccessLayers("shared", "inventory", "facultyCatalog", "professorCatalog", "courseLeadership")
+                             .check(classes);
+    }
+
+    @Test
+    void checkSubdomainsDependencies() {
+        layeredArchitecture().consideringOnlyDependenciesInLayers()
+                             .layer("availability").definedBy("com.github.maleksandrowicz93.edu.domain.availability..")
+                             .layer("inventory").definedBy("com.github.maleksandrowicz93.edu.domain.inventory..")
+                             .layer("courseCatalog").definedBy("com.github.maleksandrowicz93.edu.domain.educationalInstitution.courseCatalog..")
+                             .layer("courseCreation").definedBy("com.github.maleksandrowicz93.edu.domain.educationalInstitution.courseCreation..")
+                             .layer("courseLeadership").definedBy("com.github.maleksandrowicz93.edu.domain.educationalInstitution.courseLeadership..")
+                             .layer("facultyCatalog").definedBy("com.github.maleksandrowicz93.edu.domain.educationalInstitution.facultyCatalog..")
+                             .layer("facultyCreation").definedBy("com.github.maleksandrowicz93.edu.domain.educationalInstitution.facultyCreation..")
+                             .layer("educationalInstitutionInventory").definedBy("com.github.maleksandrowicz93.edu.domain.educationalInstitution.inventory..")
+                             .layer("professorCatalog").definedBy("com.github.maleksandrowicz93.edu.domain.educationalInstitution.professorCatalog..")
+                             .layer("professorEmployment").definedBy("com.github.maleksandrowicz93.edu.domain.educationalInstitution.professorEmployment..")
+                             .layer("shared").definedBy("com.github.maleksandrowicz93.edu.domain.educationalInstitution.shared..")
+                             .layer("studentEnrollment").definedBy("com.github.maleksandrowicz93.edu.domain.educationalInstitution.studentEnrollment..")
+                             .whereLayer("availability").mayOnlyBeAccessedByLayers("courseLeadership")
+                             .whereLayer("inventory").mayOnlyBeAccessedByLayers("educationalInstitutionInventory")
+                             .check(classes);
+    }
 }
